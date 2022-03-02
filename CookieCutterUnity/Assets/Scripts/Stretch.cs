fd462d1a981bad499d6f2974e43e89637a57fbf7
Alex Jenkins <arj0288@utulsa.edu>
2022-03-02 16:45:59 -0600

    basics

---
+++ CookieCutterUnity/Assets/Scripts/Stretch.cs
@@ -0,0 +1,35 @@
+/*
+ * Author: Alex Jenkins
+ * Date created: 3/2/22
+ * 
+ * Last updated by: Alex Jenkins
+ * Date last udpated: 3/2/22
+ * 
+ * Description: Control the Stretch of the dough
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stretch : MonoBehaviour
+{
+    static private GameObject dough;
+
+    private void Awake()
+    {
+        dough = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
